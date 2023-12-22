7d3ad26192615fcb3ab9717c0c99b253aa2c77e7
fakanakavera <34803622+fakanakavera@users.noreply.github.com>
2023-12-22 21:42:50 +0900

    templates setup

--- fnk_auth/apps.py
+++ fnk_auth/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class YourAppNameConfig(AppConfig):
+class FnkAuthConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'your_app_name'
+    name = 'fnk_auth'
--- fnk_auth/views.py
+++ fnk_auth/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def login_view(request):
+    return render(request, 'fnk_auth/login.html')
+
+
+def logout_view(request):
+    return render(request, 'fnk_auth/logout.html')
